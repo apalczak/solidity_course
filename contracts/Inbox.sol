f7253f08b1c26dbfbed6f7e8eeff1440f6be06d8
ArkPal <arkadiusz.palczak@gmail.com>
2023-03-07 23:51:54 +0100

    Add tests for the first contract

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -9,10 +9,5 @@ contract Inbox {
     
     function setMessage(string newMessage) public {
         message = newMessage;
-    }
-    
-    function getMessage() public view returns (string) {
-        return message;
-    }
-    
+    }   
 }
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,39 @@
-// contract test code will go here
+const assert = require("assert");
+const genache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(genache.provider());
+const { interface, bytecode } = require("../compile");
+
+const INITIAL_STRING = "Hello World!";
+const MESSAGE_TO_CHANGE = "Hi there!";
+
+describe("Inbox", () => {
+    let accounts;
+    let inbox;
+    beforeEach(async () => {
+        accounts = await web3.eth.getAccounts();
+        inbox = await new web3.eth.Contract(JSON.parse(interface))
+            .deploy({
+                data: bytecode,
+                arguments: [INITIAL_STRING],
+            })
+            .send({ from: accounts[0], gas: "1000000" });
+    });
+
+    it("deploys a contract", () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it("has a default message", async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING);
+    });
+
+    it("is able to set new message", async () => {
+        await inbox.methods
+            .setMessage(MESSAGE_TO_CHANGE)
+            .send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, MESSAGE_TO_CHANGE);
+    });
+});
